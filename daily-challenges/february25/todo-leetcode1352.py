eb3037e16e3ec3ac47c692d3170e2f6ee29d6045
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2025-02-14 01:34:51 -0500

    Create todo-leetcode1352.py

---
+++ daily-challenges/february25/todo-leetcode1352.py
@@ -0,0 +1,36 @@
+"""
+ # https://leetcode.com/problems/product-of-the-last-k-numbers/?envType=daily-question&envId=2025-02-14
+ # 1352. Product of the Last K Numbers
+ # Design an algorithm that accepts a stream of integers and retrieves the product of the last k integers of the stream.
+ # Implement the ProductOfNumbers class:
+   ## ProductOfNumbers() Initializes the object with an empty stream.
+   ## void add(int num) Appends the integer num to the stream.
+   ## int getProduct(int k) Returns the product of the last k numbers in the current list. 
+   ## You can assume that always the current list has at least k numbers.
+ # The test cases are generated so that, at any time, 
+ # the product of any contiguous sequence of numbers will fit into a single 32-bit integer without overflowing.
+"""
+class leetcode1352(object):
+
+    def __init__(self):
+        
+
+    def add(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        
+
+    def getProduct(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
